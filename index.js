1736708eff427f30f5585555103675f841c69b39
Erica Worthey <elworthey@gmail.com>
2023-01-01 09:41:36 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//Write a function that takes 3 parameters - name, height, and country. 
+//Write an alert in the function that prints out the inputs.
+function printInfo(name, height, country) {
+alert('Hello RA! My name is ' + name + ', I am ' + height + ' inches tall, and I live in ' + country + '.')
+}
+
+//Call the function and pass the 3 parameter values into it
+printInfo('Erica', '62', 'The United States');
+
+
